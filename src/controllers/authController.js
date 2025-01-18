6a9890dccb1b1571bebff22cc08acca4f39198e9
SOHEL RANA <sohelrana104322@gmail.com>
2025-01-18 23:42:59 +0600

    complete login section

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -31,4 +31,39 @@ const register = async (req, res) => {
   }
 };
 
-export { register };
+const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const userExist = await user.findOne({ email });
+    // console.log(userExist, "userExist");
+
+    if (!userExist) {
+      return res.status(400).json({
+        message: "Invalid Credentials",
+      });
+    }
+
+    const isPasswordValid = await userExist.comparePassword(password);
+
+    // console.log(isPasswordValid, "isPasswordValid");
+    if (isPasswordValid) {
+      return res.status(200).json({
+        message: "Successfully login",
+        token: await userExist.generateToken(),
+        userId: userExist._id.toString(),
+        email: userExist.email,
+      });
+    } else {
+      res.status(400).json({
+        message: "Invalid email or password",
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "Internal server error",
+      error: err,
+    });
+  }
+};
+
+export { register, login };
--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,10 +1,16 @@
 import express from "express";
 import usersValidatorSchema from "../validator/authValidator.js";
-import { register as authRegister } from "../controllers/authController.js";
+import {
+  register as authRegister,
+  login as authLogin,
+} from "../controllers/authController.js";
 const router = new express.Router();
 import validate from "../middlewares/validateMiddleware.js";
 
 // =====for sign up=====
 router.route("/sign-up").post(validate(usersValidatorSchema), authRegister);
+
+// =====for login====
+router.route("/login").post(authLogin);
 
 export default router;
