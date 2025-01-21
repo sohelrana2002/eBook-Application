2ffc70c4d2acf74138a7fd66630688a80dd00bf1
SOHEL RANA <sohelrana104322@gmail.com>
2025-01-21 18:56:38 +0600

    generate json web token in auth model

--- src/models/authModel.js
+++ src/models/authModel.js
@@ -55,7 +55,7 @@ userSchema.methods.comparePassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-// =====json web token=====
+// =====generate json web token=====
 userSchema.methods.generateToken = async function () {
   try {
     return jwt.sign(
