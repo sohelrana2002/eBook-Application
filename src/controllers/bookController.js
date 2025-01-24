adb024613a62bb53358fd41583a1aa1d357d1f00
SOHEL RANA <sohelrana104322@gmail.com>
2025-01-25 00:50:32 +0600

    simply for testing to create book controller

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,5 +1,43 @@
-const createBook = async (req, res) => {
-  res.json("create book");
+import booksModel from "../models/bookModel.js";
+
+const createBook = async (req, res, next) => {
+  try {
+    const {
+      title,
+      author,
+      description,
+      genre,
+      language,
+      publicationDate,
+      price,
+      tags,
+      coverImage,
+      bookFile,
+    } = req.body;
+
+    const registerBook = await booksModel.create({
+      title,
+      author,
+      description,
+      genre,
+      language,
+      publicationDate,
+      price,
+      tags,
+      coverImage,
+      bookFile,
+    });
+
+    res.status(201).json({
+      message: "Create new book successfully",
+      id: registerBook._id,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "internal server error",
+      error: err,
+    });
+  }
 };
 
 export { createBook };
