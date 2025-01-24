ee3f3dc1aa5eb66e8186ebb3abe66d07587c73c5
SOHEL RANA <sohelrana104322@gmail.com>
2025-01-25 00:51:01 +0600

    fixed issues in book router

--- src/routers/bookRouter.js
+++ src/routers/bookRouter.js
@@ -1,7 +1,9 @@
 import express from "express";
 const bookRouter = new express.Router();
 import { createBook } from "../controllers/bookController.js";
+import booksValidatorSchema from "../validator/bookValidator.js";
+import validate from "../middlewares/validateMiddleware.js";
 
-bookRouter.route("/").get(createBook);
+bookRouter.route("/").post(validate(booksValidatorSchema), createBook);
 
 export default bookRouter;
