089819ef5bd2e8170559490895ba9aff10220a64
SOHEL RANA <sohelrana104322@gmail.com>
2025-01-26 01:19:59 +0600

    organized book router

--- server/src/routers/bookRouter.js
+++ server/src/routers/bookRouter.js
@@ -3,7 +3,15 @@ const bookRouter = new express.Router();
 import { createBook } from "../controllers/bookController.js";
 import booksValidatorSchema from "../validator/bookValidator.js";
 import validate from "../middlewares/validateMiddleware.js";
+import { upload } from "../middlewares/multerMiddleware.js";
 
-bookRouter.route("/").post(validate(booksValidatorSchema), createBook);
+bookRouter.route("/").post(
+  upload.fields([
+    { name: "coverImage", maxCount: 1 },
+    { name: "bookFile", maxCount: 1 },
+  ]),
+  validate(booksValidatorSchema),
+  createBook
+);
 
 export default bookRouter;
