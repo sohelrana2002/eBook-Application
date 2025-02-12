6003888f786eab9e8dc02b4794e9d037c9a24302
SOHEL RANA <sohelrana104322@gmail.com>
2025-02-12 23:36:56 +0600

    extended url

--- server/src/app.js
+++ server/src/app.js
@@ -6,6 +6,7 @@ import bookRouter from "./routers/bookRouter.js";
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRouter);
 app.use("/api/books", bookRouter);
