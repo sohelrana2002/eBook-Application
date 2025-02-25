01f103015242c3149e49320eebc381e4d5528237
SOHEL RANA <sohelrana104322@gmail.com>
2025-02-25 14:46:10 +0600

    add: get all review router

--- server/src/routers/reviewRouter.js
+++ server/src/routers/reviewRouter.js
@@ -1,8 +1,12 @@
 import express from "express";
 const reviewRouter = new express.Router();
-import { addReview } from "../controllers/reviewController.js";
+import { addReview, allReview } from "../controllers/reviewController.js";
 import jwtAuthMiddleware from "../middlewares/jwtAuthMiddleware.js";
 
+// ----add review router----
 reviewRouter.route("/:bookId/review").post(jwtAuthMiddleware, addReview);
+
+// ----get all review router----
+reviewRouter.route("/:bookId/reviews").get(allReview);
 
 export default reviewRouter;
