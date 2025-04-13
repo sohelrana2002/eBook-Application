6752c7c87a2e6992268a99f3d1181ac30a389486
SOHEL RANA <sohelrana104322@gmail.com>
2025-04-13 22:31:58 +0600

    complete login page front-end

---
+++ admin/src/auth/login/Login.jsx
@@ -0,0 +1,62 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+const Login = () => {
+  return (
+    <div>
+      <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+        <div className="flex w-full max-w-sm flex-col gap-6">
+          <Card>
+            <CardHeader className="text-center">
+              <CardTitle className="text-xl">Welcome back</CardTitle>
+              <CardDescription>Login with admin account</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <form>
+                <div className="grid gap-6">
+                  <div className="flex flex-col gap-4"></div>
+                  <div className="grid gap-6">
+                    <div className="grid gap-2">
+                      <Label htmlFor="email">Email</Label>
+                      <Input
+                        id="email"
+                        type="email"
+                        placeholder="m@example.com"
+                        required
+                      />
+                    </div>
+                    <div className="grid gap-2">
+                      <div className="flex items-center">
+                        <Label htmlFor="password">Password</Label>
+                        <a
+                          href="#"
+                          className="ml-auto text-sm underline-offset-4 hover:underline"
+                        >
+                          Forgot your password?
+                        </a>
+                      </div>
+                      <Input id="password" type="password" required />
+                    </div>
+                    <Button type="submit" className="w-full">
+                      Login
+                    </Button>
+                  </div>
+                </div>
+              </form>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- admin/src/router/Router.jsx
+++ admin/src/router/Router.jsx
@@ -1,11 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/homePage/Home";
+import Login from "../auth/login/Login";
 
 import App from "../App";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ]);
 
