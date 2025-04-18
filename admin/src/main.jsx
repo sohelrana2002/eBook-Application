f5b1a584f8fac1dd770c9eb6677c970e3d8071b8
SOHEL RANA <sohelrana104322@gmail.com>
2025-04-18 15:16:39 +0600

    remove stick mode

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,5 +1,4 @@
 import "./index.css";
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/Router.jsx";
@@ -9,9 +8,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-  </StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>
 );
