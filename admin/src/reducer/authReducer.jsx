1722264ad0b9ed083fcebcf03b24197609b0d88a
SOHEL RANA <sohelrana104322@gmail.com>
2025-04-18 16:52:48 +0600

    create authReducer

---
+++ admin/src/reducer/authReducer.jsx
@@ -0,0 +1,26 @@
+const authReducer = (state, action) => {
+  switch (action.type) {
+    case "REMOVE_TOKEN": {
+      const removeToken = localStorage.removeItem("token");
+      localStorage.removeItem("email");
+
+      return {
+        ...state,
+        token: "",
+        removeToken,
+      };
+    }
+
+    case "SET_TOKEN_IN_STATE": {
+      return {
+        ...state,
+        token: action.payload,
+      };
+    }
+
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
