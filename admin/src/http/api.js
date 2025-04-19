7655f442dde32b14cc59d9013697f9c68ff2bd02
SOHEL RANA <sohelrana104322@gmail.com>
2025-04-20 00:49:38 +0600

    add createBook api call

--- admin/src/http/api.js
+++ admin/src/http/api.js
@@ -35,3 +35,12 @@ export const listBooks = async () => {
   const res = await api.get("/api/books");
   return res.data;
 };
+
+export const createBook = async (formData) => {
+  const res = await api.post("/api/books", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+  return res.data;
+};
