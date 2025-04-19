7e032db2d1889e0dd357990124721bf2bed981d5
SOHEL RANA <sohelrana104322@gmail.com>
2025-04-19 16:27:07 +0600

    create custom loading

---
+++ admin/src/shared/loading/Loading.jsx
@@ -0,0 +1,11 @@
+import "./Loading.css";
+
+const Loading = () => {
+  return (
+    <div className="loading__container">
+      <span class="loader"></span>
+    </div>
+  );
+};
+
+export default Loading;
