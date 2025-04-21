4ae0e896dc7cadaf6ffee4dd37a424b92a59d2ed
SOHEL RANA <sohelrana104322@gmail.com>
2025-04-22 01:11:52 +0600

    fixed some issue

--- server/src/middlewares/multerMiddleware.js
+++ server/src/middlewares/multerMiddleware.js
@@ -1,6 +1,7 @@
 import multer from "multer";
 import path from "path";
 import { fileURLToPath } from "url";
+import express from "express";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
