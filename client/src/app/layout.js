4e3857b2c657fe81f83108c106ada3dee50ccb17
SOHEL RANA <sohelrana104322@gmail.com>
2025-04-27 22:46:21 +0600

    setup next js project and initial files and folders

---
+++ client/src/app/layout.js
@@ -0,0 +1,27 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "e-Book Application",
+  description: "e-Book platform",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" data-arp="">
+      <body data-new-gr-c-s-check-loaded="14.1232.0" data-gr-ext-installed="">
+        {children}
+      </body>
+    </html>
+  );
+}
