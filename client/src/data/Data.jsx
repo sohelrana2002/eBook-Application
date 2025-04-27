cf50792b8f753ffc896fbea157c43d9e8a519c43
SOHEL RANA <sohelrana104322@gmail.com>
2025-04-28 00:52:42 +0600

    data for navbar

---
+++ client/src/data/Data.jsx
@@ -0,0 +1,27 @@
+export const navMenu = [
+  {
+    id: 1,
+    path: "/",
+    title: "home",
+  },
+  {
+    id: 2,
+    path: "/books",
+    title: "books",
+  },
+  {
+    id: 3,
+    path: "/about-us",
+    title: "about us",
+  },
+  {
+    id: 4,
+    path: "/blogs",
+    title: "blogs",
+  },
+  {
+    id: 5,
+    path: "/contact",
+    title: "contact",
+  },
+];
