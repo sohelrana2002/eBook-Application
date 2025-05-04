722e8ecc70e22c32fb00d3a37ffce7f79e81b055
SOHEL RANA <sohelrana104322@gmail.com>
2025-05-04 23:43:20 +0600

    create context for auth

---
+++ client/src/context/authContext.jsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { createContext, useContext, useReducer } from "react";
+import authReducer from "@/reducer/authReducer";
+
+// ====create contect======
+const AuthContext = createContext();
+
+const initialState = {
+  token: typeof window !== "undefined" ? localStorage.getItem("token") : "",
+};
+
+// ====create provider====
+const AuthProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, initialState);
+
+  // ======store token in local storage====
+  const storeTokenInLS = (serverToken, name) => {
+    dispatch({
+      type: "SET_TOKEN_IN_STATE",
+      payload: serverToken,
+    });
+    localStorage.setItem("name", name);
+    return localStorage?.setItem("token", serverToken);
+  };
+
+  // =======user log out =====
+  const logOutUser = () => {
+    dispatch({
+      type: "REMOVE_TOKEN",
+    });
+  };
+
+  let isLoggedIn = state.token;
+
+  const value = {
+    ...state,
+    dispatch,
+    storeTokenInLS,
+    logOutUser,
+    isLoggedIn,
+  };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+// =====custom hook========
+const useAuthContext = () => {
+  return useContext(AuthContext);
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export { AuthProvider, useAuthContext };
