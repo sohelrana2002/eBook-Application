e126b9cd9b8c72d71ade09668620801f05782608
SOHEL RANA <sohelrana104322@gmail.com>
2025-05-04 23:46:01 +0600

    worpping globally

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/shared/navbar/Navbar";
+import { AuthProvider } from "@/context/authContext";
+import TanstackProvider from "@/provider/TanstackProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,8 +23,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" data-arp="">
       <body data-new-gr-c-s-check-loaded="14.1233.0" data-gr-ext-installed="">
-        <Navbar />
-        <div className="pt-[6rem]">{children}</div>
+        <AuthProvider>
+          <TanstackProvider>
+            <Navbar />
+            <div className="pt-[4.5rem]">{children}</div>
+          </TanstackProvider>
+        </AuthProvider>
       </body>
     </html>
   );
