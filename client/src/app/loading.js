a955af2e1099bc35a635ee0a1ab33c7c46254d29
SOHEL RANA <sohelrana104322@gmail.com>
2025-05-05 21:28:54 +0600

    loadig state

--- client/src/app/loading.js
+++ client/src/app/loading.js
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div className="loading__container">
-      <span class="loader"></span>
+      <span className="loader"></span>
     </div>
   );
 }
