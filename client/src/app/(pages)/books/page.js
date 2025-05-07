d10cdb67d2f7d1e4b7e0cf5db8f32997b68e678b
SOHEL RANA <sohelrana104322@gmail.com>
2025-05-08 00:05:15 +0600

    starting book page

--- client/src/app/(pages)/books/page.js
+++ client/src/app/(pages)/books/page.js
@@ -1,7 +1,9 @@
-import React from "react";
+import BookPage from "@/components/bookPage/BookPage";
+import { listBooks } from "@/http/api";
 
-const Books = () => {
-  return <div>Books</div>;
+const Books = async () => {
+  const allBooks = await listBooks();
+  return <BookPage allBooks={allBooks} />;
 };
 
 export default Books;
