ae74bc9d60dc1f3a86728436f94c17eb47e5e61a
SOHEL RANA <sohelrana104322@gmail.com>
2025-05-26 01:16:53 +0600

    book request validator

---
+++ server/src/validator/bookRequestValidator.js
@@ -0,0 +1,37 @@
+import z from "zod";
+
+const bookRequestValidatorSchema = z.object({
+  userId: z.string({ required_error: "User Id must be required." }).optional(),
+
+  bookName: z
+    .string({ required_error: "Bookname must be required." })
+    .min(1, { message: "Bookname must be at least 1 character" })
+    .trim()
+    .toLowerCase(),
+
+  authorName: z
+    .string({ required_error: "Author name must be required." })
+    .min(1, { message: "Author name must be at least 1 character" })
+    .trim()
+    .toLowerCase(),
+
+  publicationDate: z.preprocess(
+    (arg) =>
+      typeof arg === "string" || arg instanceof Date ? new Date(arg) : arg,
+    z.date({ invalid_type_error: "Publication date must be a valid date" })
+  ),
+
+  language: z
+    .string({ required_error: "Language must be required." })
+    .min(1, { message: "Language must be at least 1 character" })
+    .trim()
+    .toLowerCase(),
+
+  status: z
+    .enum(["pending", "in-progress", "added"], {
+      required_error: "status must be required",
+    })
+    .default("pending"),
+});
+
+export default bookRequestValidatorSchema;
