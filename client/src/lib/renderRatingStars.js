c06492e58d1e8007a167b6198381f880859baa50
SOHEL RANA <sohelrana104322@gmail.com>
2025-05-25 18:59:35 +0600

    add render rating funtionality

---
+++ client/src/lib/renderRatingStars.js
@@ -0,0 +1,25 @@
+import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
+
+export const renderRatingStars = (rating) => {
+  const stars = [];
+  const fullStars = Math.floor(rating);
+  const hasHalfStar = rating % 1 >= 0.5;
+
+  for (let i = 1; i <= 5; i++) {
+    if (i <= fullStars) {
+      stars.push(
+        <FaStar key={i} size={20} className="text-yellow-400 text-2xl" />
+      );
+    } else if (i === fullStars + 1 && hasHalfStar) {
+      stars.push(
+        <FaStarHalfAlt key={i} size={20} className="text-yellow-400 text-2xl" />
+      );
+    } else {
+      stars.push(
+        <FaRegStar key={i} size={20} className="text-gray-300 text-2xl" />
+      );
+    }
+  }
+
+  return stars;
+};
