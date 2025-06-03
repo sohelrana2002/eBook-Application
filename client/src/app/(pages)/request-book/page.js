c5fef50bd5b2f8b2d5b01d90f7b667f1e4d63f93
SOHEL RANA <sohelrana104322@gmail.com>
2025-06-03 22:58:17 +0600

    add request a book page

---
+++ client/src/app/(pages)/request-book/page.js
@@ -0,0 +1,11 @@
+import RequestBookPage from "@/components/requestBookPage/RequestBookPage";
+
+const RequestBook = () => {
+  return (
+    <div>
+      <RequestBookPage />
+    </div>
+  );
+};
+
+export default RequestBook;
