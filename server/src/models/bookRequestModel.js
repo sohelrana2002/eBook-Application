5ca5f0eb1ab8d25c9fd91f07f1d2d41986391a2e
SOHEL RANA <sohelrana104322@gmail.com>
2025-06-12 16:28:14 +0600

    change added to available

--- server/src/models/bookRequestModel.js
+++ server/src/models/bookRequestModel.js
@@ -26,7 +26,7 @@ const bookRequestSchema = new mongoose.Schema(
     },
     status: {
       type: String,
-      enum: ["pending", "in-progress", "added", "not-found"],
+      enum: ["pending", "in-progress", "available", "not-found"],
       default: "pending",
     },
   },
