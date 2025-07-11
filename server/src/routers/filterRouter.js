4060a9d4390a29e900ec0416541f0b5794827757
SOHEL RANA <sohelrana104322@gmail.com>
2025-07-11 23:55:18 +0600

    allgenres controller

--- server/src/routers/filterRouter.js
+++ server/src/routers/filterRouter.js
@@ -1,7 +1,12 @@
 import express from "express";
 const filterRouter = new express.Router();
-import { allAuthor, allLanguage } from "../controllers/filterController.js";
+import {
+  allAuthor,
+  allLanguage,
+  allGenre,
+} from "../controllers/filterController.js";
 
+filterRouter.route("/genres").get(allGenre);
 filterRouter.route("/authors").get(allAuthor);
 filterRouter.route("/language").get(allLanguage);
 
