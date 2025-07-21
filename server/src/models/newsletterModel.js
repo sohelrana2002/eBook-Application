4e8a8307aed1416bf7190463506d5825f3640623
SOHEL RANA <sohelrana104322@gmail.com>
2025-07-21 22:10:03 +0600

    create newsletter model

---
+++ server/src/models/newsletterModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const newsletterSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    subscribedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
+
+const newsletterModel = new mongoose.model("newsletter", newsletterSchema);
+
+export default newsletterModel;
