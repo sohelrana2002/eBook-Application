c75e7d0fa527f2ec38b50d0636c46fa4890073bd
SOHEL RANA <sohelrana104322@gmail.com>
2025-08-02 17:51:29 +0600

    .env config

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -6,6 +6,9 @@ const _config = {
   databaseURI: process.env.MONGOOSE_STRING_URI,
   errorStackEnv: process.env.NODE_ENV,
   jwtSecretKey: process.env.JWT_SECRET_KEY,
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  googleCallBackURL: process.env.GOOGLE_CALLBACK_URL,
 };
 
 // ---freeze method used for only read purpose----
