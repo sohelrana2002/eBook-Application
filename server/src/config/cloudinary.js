e3747d9ad165b2b92dfa174566f6acb8544fcf93
SOHEL RANA <sohelrana104322@gmail.com>
2025-08-10 23:45:57 +0600

    fixed imported issues

--- server/src/config/cloudinary.js
+++ server/src/config/cloudinary.js
@@ -1,5 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
-import { config } from "./config";
+import { config } from "./config.js";
 
 cloudinary.config({
   cloud_name: config.cloudinaryCloud,
