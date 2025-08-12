b784abf234aee51616c16e3c8f4b55e1849b2bef
SOHEL RANA <sohelrana104322@gmail.com>
2025-08-12 12:59:20 +0600

    access the image protocal domain

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "", // leave empty unless you have a custom port
+        pathname: "/**", // allow all paths
+      },
+    ],
+  },
 };
 
 export default nextConfig;
