bc865bcc36f52b5aaad6fe053130eb165d8900f9
SOHEL RANA <sohelrana104322@gmail.com>
2025-12-24 00:22:58 +0600

    complete study about jwt decode

--- client/src/app/(pages)/decode/page.js
+++ client/src/app/(pages)/decode/page.js
@@ -1,21 +1,25 @@
+// this study is a part of big project
+
 "use client";
 
 import React from "react";
 import { jwtDecode } from "jwt-decode";
 
 const Decode = () => {
-  //   const { isLoggedIn } = useAuthContext();
   const token =
     typeof window !== "undefined" ? localStorage.getItem("token") : "";
 
   const decoded = jwtDecode(token);
-
-  console.log("decoded", decoded);
+  //   console.log("decoded", decoded);
 
   return (
-    <div>
-      <h1>Decode history</h1>
-      {token}
+    <div className="p-[5%]">
+      <h1>Study about JWT decoding.</h1>
+      <h3 className="mt-[30px]">Id: {decoded.userId}</h3>
+      <h3>Name: {decoded.name}</h3>
+      <h3>Role: {decoded.role}</h3>
+
+      {decoded.role === "admin" ? "Hello I'm admin" : "Hello I'm not admin"}
     </div>
   );
 };
