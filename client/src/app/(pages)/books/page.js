43af7692fcac71e9c732946348914c479953609d
SOHEL RANA <sohelrana104322@gmail.com>
2026-01-15 16:33:29 +0600

    forcefully make this page dynamic

--- client/src/app/(pages)/books/page.js
+++ client/src/app/(pages)/books/page.js
@@ -1,6 +1,8 @@
 import BookPage from "@/components/bookPage/BookPage";
 import { listBooks } from "@/lib/api";
 
+export const dynamic = "force-dynamic";
+
 const Books = async () => {
   const allBooks = await listBooks();
   return <BookPage allBooks={allBooks} />;
