019c590527dee1e81581d93fbb2b33e851242887
SOHEL RANA <sohelrana104322@gmail.com>
2026-01-23 06:41:18 +0600

    payment success page

--- client/src/app/(pages)/payment-success/page.js
+++ client/src/app/(pages)/payment-success/page.js
@@ -1,7 +1,24 @@
-import React from "react";
+// pages/payment-success.jsx
+"use client";
+
+import { useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+import { api } from "@/lib/api";
 
 const PaymentSuccess = () => {
-  return <div>PaymentSuccess</div>;
+  const searchParams = useSearchParams();
+  const tranId = searchParams.get("tran_id");
+
+  useEffect(() => {
+    if (tranId) {
+      api
+        .post("/api/payment/success", { tran_id: tranId })
+        .then((res) => console.log("Payment verified:", res.data))
+        .catch((err) => console.error(err));
+    }
+  }, [tranId]);
+
+  return <div>Payment Success! Your eBook will be available soon.</div>;
 };
 
 export default PaymentSuccess;
