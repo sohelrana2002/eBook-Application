099645fd33e1ecf273a373e407586baf79502049
SOHEL RANA <sohelrana104322@gmail.com>
2026-07-05 14:09:29 +0600

    Made it more pretty card

--- client/src/components/bookCard/BookCard.jsx
+++ client/src/components/bookCard/BookCard.jsx
@@ -1,38 +1,55 @@
-import { renderRatingStars } from "@/lib/renderRatingStars";
 import Image from "next/image";
 const defaultImage = "/book.jpg";
+import { FaStar } from "react-icons/fa";
 
-const BookCard = ({ coverImage, title, averageRating, price, author }) => {
+import Link from "next/link";
+import CardButton from "../cardButton/CardButton";
+
+const BookCard = ({
+  _id,
+  slug,
+  coverImage,
+  title,
+  averageRating,
+  price,
+  author,
+}) => {
   return (
-    <div className="w-full  md:max-w-xs bg-white shadow-lg rounded-2xl  hover:shadow-xl transition-shadow duration-300 border border-[var(--border)] overflow-auto">
-      {/* Image wrapper */}
-      <div className="relative w-full h-60">
-        <Image
-          src={coverImage || defaultImage}
-          alt={title}
-          fill
-          style={{ objectFit: "cover" }}
-          className="rounded-t-2xl"
-        />
-      </div>
-
-      <div className="p-4 space-y-2 border-t-2 border-[var(--border)] flex items-start justify-between">
-        <div>
-          <h2 className="text-[17px] font-semibold text-gray-800 line-clamp-2 capitalize max-w-[200px] text-left">
-            {title.length > 12 ? `${title.substring(0, 12)}...` : title}
-          </h2>
-
-          <h2 className="text-[14px] font-semibold text-gray-800 line-clamp-2 capitalize text-left">
-            {author.length > 12 ? `${author.substring(0, 12)}...` : author}
-          </h2>
+    <div className="w-full  md:max-w-xs bg-[var(--light-gray)] shadow-lg rounded-lg  hover:shadow-xl transition-shadow duration-300 border-2 border-[var(--border)] overflow-hidden flex flex-col justify-between">
+      <Link href={`/books/${slug}`} className="block flex-1">
+        {/* Image wrapper */}
+        <div className="relative w-full h-40">
+          <Image
+            src={coverImage || defaultImage}
+            alt={title}
+            fill
+            style={{ objectFit: "cover" }}
+            className="rounded-t-lg"
+          />
         </div>
-        <div className="flex flex-col items-end">
-          <p className="text-[17px] font-bold text-[var(--blue)]">${price}</p>
-          <div className="flex items-center gap-1 text-yellow-500">
-            {renderRatingStars(averageRating)}
+
+        <div className="p-[10px] space-y-1 border-t-2 border-[var(--border)]">
+          <div>
+            <h2 className="text-[17px] font-bold text-[var(--dark)] line-clamp-2 uppercase text-left">
+              {title}
+            </h2>
+
+            <h2 className="text-[13.5px] font-semibold text-[var(--medium-gray)] line-clamp-2 capitalize text-left">
+              {author}
+            </h2>
+          </div>
+          <div className="flex items-center justify-between">
+            <p className="text-[17px] font-bold text-[var(--blue)]">${price}</p>
+            <div className="flex items-center gap-1 text-yellow-500">
+              <FaStar className="text-yellow-500 text-[17px]" />
+              <span className="text-[17px] font-medium">{averageRating}</span>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
+
+      {/* bottom section  */}
+      <CardButton bookid={_id} />
     </div>
   );
 };
