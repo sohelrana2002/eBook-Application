3dff0f336316c6604ea8ea004da15c229940f870
SOHEL RANA <sohelrana104322@gmail.com>
2026-07-05 11:37:39 +0600

    Improve table style

--- client/src/components/requestedBookPage/RequestedBookPage.jsx
+++ client/src/components/requestedBookPage/RequestedBookPage.jsx
@@ -21,7 +21,7 @@ const RequestedBookPage = () => {
     },
     onError: (error) => {
       alert(
-        error?.response?.data?.message || "Failed to delete requested book"
+        error?.response?.data?.message || "Failed to delete requested book",
       );
     },
   });
@@ -38,88 +38,93 @@ const RequestedBookPage = () => {
   const books = data?.allBookRequest || [];
 
   return (
-    <div className="min-h-screen md:p-6">
-      <div className="max-w-6xl mx-auto bg-white shadow-lg rounded-lg overflow-x-auto">
-        <div className="px-6 py-4 border-b">
-          <h2 className="text-xl font-semibold text-gray-800">
+    <div className="max-w-6xl w-full mx-auto bg-[var(--border)] shadow-lg rounded-lg">
+      <div className="p-5">
+        <div className="py-4">
+          <h2 className="text-xl font-semibold text-[var(--black)]">
             Requested Books
           </h2>
         </div>
 
-        <table className="min-w-full divide-y divide-gray-200">
-          <thead className="bg-gray-50">
-            <tr>
-              <th className="px-6 py-3 text-left text-xs font-semibold text-gray-600 uppercase">
-                Book Name
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-semibold text-gray-600 uppercase">
-                Author
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-semibold text-gray-600 uppercase">
-                Pub. Date
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-semibold text-gray-600 uppercase">
-                Language
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-semibold text-gray-600 uppercase">
-                Status
-              </th>
-              <th className="px-6 py-3 text-left text-xs font-semibold text-gray-600 uppercase">
-                Action
-              </th>
-            </tr>
-          </thead>
-
-          <tbody className="bg-white divide-y divide-gray-200">
-            {/* NO BOOK MESSAGE */}
-            {books.length === 0 ? (
-              <tr>
-                <td
-                  colSpan="6"
-                  className="text-center py-6 text-gray-500 text-sm"
-                >
-                  No book request found
-                </td>
+        <div className="overflow-x-auto">
+          <table className="w-full table-auto divide-y divide-gray-200 overflow-hidden rounded-lg border-none border-spacing-0">
+            <thead className="bg-gray-50">
+              <tr className="bg-black text-white whitespace-nowrap">
+                <th className="px-4 md:px-6 py-2 md:py-3 text-left text-[8px] md:text-xs font-semibold  uppercase">
+                  Book Name
+                </th>
+                <th className="px-4 md:px-6 py-2 md:py-3 text-left text-[8px] md:text-xs  uppercase">
+                  Author
+                </th>
+                <th className="px-4 md:px-6 py-2 md:py-3 text-left text-[8px] md:text-xs  uppercase">
+                  Pub. Date
+                </th>
+                <th className="px-4 md:px-6 py-2 md:py-3 text-left text-[8px] md:text-xs  uppercase">
+                  Language
+                </th>
+                <th className="px-4 md:px-6 py-2 md:py-3 text-left text-[8px] md:text-xs  uppercase">
+                  Status
+                </th>
+                <th className="px-4 md:px-6 py-2 md:py-3 text-left text-[8px] md:text-xs uppercase">
+                  Action
+                </th>
               </tr>
-            ) : (
-              books.map((book) => (
-                <tr key={book._id} className="hover:bg-gray-50 transition">
-                  <td className="capitalize px-6 py-4 text-sm font-medium text-gray-800">
-                    {book.bookName}
-                  </td>
-                  <td className="capitalize px-6 py-4 text-sm text-gray-700">
-                    {book.authorName}
-                  </td>
-                  <td className="px-6 py-4 text-sm text-gray-500">
-                    {new Date(book.publicationDate).toLocaleDateString()}
-                  </td>
-                  <td className="capitalize px-6 py-4 text-sm text-gray-500">
-                    {book.language}
-                  </td>
-                  <td className="px-6 py-4">
-                    <span
-                      className={`capitalize inline-block px-2 py-1 rounded-full text-xs font-semibold ${
-                        book.status === "available"
-                          ? "bg-green-100 text-green-700"
-                          : "bg-red-100 text-red-700"
-                      }`}
-                    >
-                      {book.status}
-                    </span>
-                  </td>
-                  <td className="px-6 py-4">
-                    <button
-                      onClick={() => handleDelete(book._id)}
-                      className="rounded bg-red-600 px-3 py-1 text-white text-xs hover:bg-red-700"
-                    >
-                      Delete
-                    </button>
+            </thead>
+
+            <tbody className="bg-white divide-y divide-gray-200">
+              {/* NO BOOK MESSAGE */}
+              {books.length === 0 ? (
+                <tr>
+                  <td
+                    colSpan="6"
+                    className="text-center py-6 text-gray-500 text-sm"
+                  >
+                    No book request found
                   </td>
                 </tr>
-              ))
-            )}
-          </tbody>
-        </table>
+              ) : (
+                books.map((book) => (
+                  <tr
+                    key={book._id}
+                    className="hover:bg-gray-100 transition whitespace-nowrap"
+                  >
+                    <td className="capitalize px-3 md:px-6 py-2 md:py-4 text-[8px] md:text-sm font-medium text-[var(--black)]">
+                      {book.bookName}
+                    </td>
+                    <td className="capitalize px-3 md:px-6 py-2 md:py-4 text-[8px] md:text-sm text-[var(--black)]">
+                      {book.authorName}
+                    </td>
+                    <td className="px-3 md:px-6 py-2 md:py-4 text-[8px] md:text-sm text-[var(--black)]">
+                      {new Date(book.publicationDate).toLocaleDateString()}
+                    </td>
+                    <td className="capitalize px-3 md:px-6 py-2 md:py-4 text-[8px] md:text-sm text-[var(--black)]">
+                      {book.language}
+                    </td>
+                    <td className="px-3 md:px-6 py-2 md:py-4">
+                      <span
+                        className={`capitalize inline-block px-2 py-1 rounded-full text-[8px] md:text-xs font-semibold ${
+                          book.status === "available"
+                            ? "bg-green-100 text-green-700"
+                            : "bg-red-100 text-red-700"
+                        }`}
+                      >
+                        {book.status}
+                      </span>
+                    </td>
+                    <td className="px-3 md:px-6 py-2 md:py-4">
+                      <button
+                        onClick={() => handleDelete(book._id)}
+                        className="rounded  cursor-pointer bg-red-600 px-3 py-1 text-white  text-[8px] md:text-sm hover:bg-red-700"
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))
+              )}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
