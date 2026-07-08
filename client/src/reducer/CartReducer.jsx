135569a2369a3084cabb43662f17a2f12b66511d
SOHEL RANA <sohelrana104322@gmail.com>
2026-07-09 01:36:53 +0600

    cart reducer

---
+++ client/src/reducer/CartReducer.jsx
@@ -0,0 +1,41 @@
+const CartReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TO_CART": {
+      const updatedCarts = [...state.carts, action.payload];
+      const totalQuantity = updatedCarts.length;
+      const totalPrice = updatedCarts.reduce(
+        (sum, book) => sum + book.price,
+        0,
+      );
+
+      return { carts: updatedCarts, totalQuantity, totalPrice };
+    }
+
+    case "REMOVE_FROM_CART": {
+      const updatedCarts = state.carts.filter(
+        (book) => book?._id !== action.payload,
+      );
+
+      const totalQuantity = updatedCarts.length;
+      const totalPrice = updatedCarts.reduce(
+        (sum, book) => sum + book.price,
+        0,
+      );
+
+      return { carts: updatedCarts, totalQuantity, totalPrice };
+    }
+
+    case "SET_CART": {
+      return action.payload;
+    }
+
+    case "CLEAR_CART": {
+      return action.payload;
+    }
+
+    default:
+      return state;
+  }
+};
+
+export default CartReducer;
