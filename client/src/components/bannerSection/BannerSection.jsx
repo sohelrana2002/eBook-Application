902a60dbcdc04988507b485510cd09275571d59b
SOHEL RANA <sohelrana104322@gmail.com>
2026-07-13 22:44:48 +0600

    Banner image optimization

--- client/src/components/bannerSection/BannerSection.jsx
+++ client/src/components/bannerSection/BannerSection.jsx
@@ -1,9 +1,21 @@
 import "./BannerSection.css";
 import Link from "next/link";
+import Image from "next/image";
 
 const BannerSection = () => {
   return (
     <div className="banner__section">
+      <Image
+        src="/book-library.jpg"
+        alt="Banner image"
+        fill
+        priority
+        sizes="100%"
+        className="object-cover object-center"
+      />
+
+      <div className="banner__section__overlay" />
+
       <div className="banner__section-content">
         <h1>Smart Reading Starts Here.</h1>
         <Link href="/books" className="btn">
