60ae441e134fb5cd65e2d9bb538bc85816cd4737
SOHEL RANA <sohelrana104322@gmail.com>
2026-07-17 23:04:10 +0600

    Modify dropdown style

--- client/src/shared/profileDropDown/ProfileDropDown.jsx
+++ client/src/shared/profileDropDown/ProfileDropDown.jsx
@@ -2,6 +2,7 @@ import React, { useState, useRef, useEffect } from "react";
 import Link from "next/link";
 import { useAuth } from "@/hooks/useAuth";
 import { useRouter } from "next/navigation";
+import { ChevronDown, CircleUserRound, LogOut } from "lucide-react";
 
 const ProfileDropdown = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -37,27 +38,35 @@ const ProfileDropdown = () => {
   return (
     <div className="relative inline-block text-left" ref={dropdownRef}>
       <button
-        onClick={toggleDropdown}
-        className="flex items-center justify-center rounded-full bg-[#000] w-[35px] h-[35px] text-md font-medium text-white hover:bg-[#000000c9] focus:outline-none cursor-pointer capitalize"
+        onMouseEnter={toggleDropdown}
+        className="flex items-center gap-1 rounded-full bg-[#000] px-4 py-2 text-sm font-medium text-white hover:bg-[#000000c9] focus:outline-none cursor-pointer capitalize"
       >
-        {name?.charAt(0)?.toUpperCase()}
+        {name?.toUpperCase()}
+        <ChevronDown />
       </button>
 
       {isOpen && (
-        <div className="absolute overflow-hidden right-0 z-10 mt-2 w-48 origin-top-right rounded-xl bg-white shadow-lg ring-1 ring-gray-400 ring-opacity-5">
-          <div className="py-1">
-            <Link
-              href="/profile"
-              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-            >
-              View Profile
-            </Link>
-            <button
-              className="cursor-pointer block w-full text-left px-4 py-2 text-sm text-red-600 hover:bg-gray-100"
-              onClick={handleLogOut}
-            >
-              Logout
-            </button>
+        <div className="absolute overflow-hidden right-0 z-10 mt-2 w-48 origin-top-right rounded-sm bg-white shadow-lg border-2 border-[var(--border)]">
+          <div className="flex flex-col gap-1 py-2">
+            <div className="flex items-center gap-3 px-4 py-2 w-full float-start text-black hover:bg-gray-100 cursor-pointer">
+              <CircleUserRound />
+              <Link
+                href="/profile"
+                className="text-sm font-semibold text-gray-700"
+              >
+                Account Info
+              </Link>
+            </div>
+
+            <div className="flex items-center gap-3 px-4 py-2 w-full float-start text-black hover:bg-gray-100 cursor-pointer">
+              <LogOut />
+              <button
+                className="text-sm font-semibold text-gray-700"
+                onClick={handleLogOut}
+              >
+                Logout
+              </button>
+            </div>
           </div>
         </div>
       )}
