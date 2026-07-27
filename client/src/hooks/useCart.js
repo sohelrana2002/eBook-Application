a0b6274016414261e5b2c7ef3441b16673184b2d
SOHEL RANA <sohelrana104322@gmail.com>
2026-07-27 23:55:33 +0600

    Fixed spelling mistake

--- client/src/hooks/useCart.js
+++ client/src/hooks/useCart.js
@@ -5,7 +5,7 @@ export const useCart = () => {
   const context = useContext(CartContext);
 
   if (!context) {
-    throw new Error("useCart must be used within a CartProvideer.");
+    throw new Error("useCart must be used within a CartProvider.");
   }
 
   return context;
