047d7c1a5a42af7ebd7fb8ec1316771278148a28
SOHEL RANA <sohelrana104322@gmail.com>
2026-07-29 23:50:54 +0600

    Modify custom hooks

--- client/src/hooks/useServerStatus.js
+++ client/src/hooks/useServerStatus.js
@@ -2,10 +2,22 @@ import { ServerContext } from "@/context/ServerContext";
 import { useContext } from "react";
 
 export const useServerStatus = () => {
+  const enableServerGate =
+    process.env.NEXT_PUBLIC_ENABLE_SERVER_GATE === "true";
+
   const context = useContext(ServerContext);
 
-  if (!context) {
+  //  Production Safety Check
+  if (!context && enableServerGate) {
     throw new Error("useServerStatus must be used within a ServerProvider.");
+  }
+
+  // Development Mode
+  if (!enableServerGate) {
+    return {
+      serverReady: true,
+      isLoading: false,
+    };
   }
 
   return context;
