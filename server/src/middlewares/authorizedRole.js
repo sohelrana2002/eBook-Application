f5db15679f9c4e7c7a3bb08859c7673872be8506
SOHEL RANA <sohelrana104322@gmail.com>
2026-08-09 03:09:23 +0600

    Make it async and fetch user role info

--- server/src/middlewares/authorizedRole.js
+++ server/src/middlewares/authorizedRole.js
@@ -1,7 +1,28 @@
+import user from "../models/auth.model.js";
+
 const authorizedRoles = (...allParams) => {
-  return (req, res, next) => {
-    if (!allParams.includes(req.jwtPayload.role)) {
+  return async (req, res, next) => {
+    const userId = req.jwtPayload?.userId;
+
+    if (!userId) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized access. No user ID found.",
+      });
+    }
+
+    const userRole = await user.findById(userId).select("role");
+
+    if (!userRole) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found!",
+      });
+    }
+
+    if (!allParams.includes(userRole.role)) {
       return res.status(403).json({
+        success: false,
         message: "Access denied.",
       });
     }
