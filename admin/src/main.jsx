5d950f8aabedc21e31e7f6d9be8297453535abe7
SOHEL RANA <sohelrana104322@gmail.com>
2026-08-12 00:11:00 +0600

    Added TooltipProvider

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -4,6 +4,7 @@ import { RouterProvider } from "react-router-dom";
 import router from "./router/Router.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "./context/authContext";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 // Create a client
 const queryClient = new QueryClient();
@@ -11,7 +12,9 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
     <AuthProvider>
-      <RouterProvider router={router} />
+      <TooltipProvider>
+        <RouterProvider router={router} />
+      </TooltipProvider>
     </AuthProvider>
-  </QueryClientProvider>
+  </QueryClientProvider>,
 );
