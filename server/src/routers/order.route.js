ebe971dcb2f9e43ee6b0ced0bed66b892ef8f3f3
SOHEL RANA <sohelrana104322@gmail.com>
2026-08-16 02:23:38 +0600

    Create order list route for admin

--- server/src/routers/order.route.js
+++ server/src/routers/order.route.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import jwtAuthMiddleware from "../middlewares/jwtAuthMiddleware.js";
 import authorizedRoles from "../middlewares/authorizedRole.js";
-import { orderInfoById } from "../controllers/order.controller.js";
+import { orderInfoById, orderList } from "../controllers/order.controller.js";
 
 const router = Router();
 
-// ORDER LIST
-router.get("/", jwtAuthMiddleware, authorizedRoles("admin", orderList));
+// ORDER LIST (ONLY ADMIN)
+router.get("/", jwtAuthMiddleware, authorizedRoles("admin"), orderList);
 
 // ORDER INFO BY ID (ONLY ADMIN)
 router.get("/:id", jwtAuthMiddleware, authorizedRoles("admin"), orderInfoById);
