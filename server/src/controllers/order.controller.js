05e780f9a9171993a04de4f28b8a4a69e073da2e
SOHEL RANA <sohelrana104322@gmail.com>
2026-08-15 23:58:37 +0600

    Create order controller

---
+++ server/src/controllers/order.controller.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+import Order from "../models/order.model.js";
+import { customMessage } from "../constants/customMessage.js";
+
+const orderInfoByIdOnlyAdmin = async (req, res) => {
+  try {
+    const orderId = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(orderId)) {
+      return res.status(409).json({
+        success: false,
+        message: customMessage.invalidId("Mongoose", orderId),
+      });
+    }
+
+    const orderData = await Order.findById(orderId).populate([
+      {
+        path: "user",
+        select: "name email phoneNumber -_id",
+      },
+      {
+        path: "book",
+        select: "title author description language publicationDate price -_id",
+      },
+    ]);
+
+    if (!orderData) {
+      return res.status(409).json({
+        success: false,
+        message: customMessage.notFound("Order", orderId),
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: customMessage.found("Order", orderId),
+      data: { orderData },
+    });
+  } catch (error) {
+    console.error("Order info by ID error: ", error.message);
+
+    res.status(500).json({
+      success: false,
+      message: customMessage.serverError(),
+    });
+  }
+};
+
+export { orderInfoByIdOnlyAdmin };
