577fecd1c1f7b081084e400c40fefedf15ea9d11
SOHEL RANA <sohelrana104322@gmail.com>
2026-08-16 06:07:43 +0600

    Create get order for users route

--- server/src/routers/order.route.js
+++ server/src/routers/order.route.js
@@ -1,12 +1,24 @@
 import { Router } from "express";
 import jwtAuthMiddleware from "../middlewares/jwtAuthMiddleware.js";
 import authorizedRoles from "../middlewares/authorizedRole.js";
-import { orderInfoById, orderList } from "../controllers/order.controller.js";
+import {
+  getMyOrders,
+  orderInfoById,
+  orderList,
+} from "../controllers/order.controller.js";
 
 const router = Router();
 
 // ORDER LIST (ONLY ADMIN)
 router.get("/", jwtAuthMiddleware, authorizedRoles("admin"), orderList);
+
+// INDIVIDUAL ORDER INFO FOR USERS
+router.get(
+  "/my-orders",
+  jwtAuthMiddleware,
+  authorizedRoles("admin", "user"),
+  getMyOrders,
+);
 
 // ORDER INFO BY ID (ONLY ADMIN)
 router.get("/:id", jwtAuthMiddleware, authorizedRoles("admin"), orderInfoById);
